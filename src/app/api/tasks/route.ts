db2fffdb5d246743e34909e3c2edd01fa219800c
Takumi Fukuzawa <forthune.0517@gmail.com>
2024-09-23 21:34:04 +0900

    39 タスク一覧取得APIの実装

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,17 @@
+import { TaskDocument, TaskModel } from "@/models/task";
+import { connectDb } from "@/utils/database"
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+    try {
+        await connectDb();
+        const allTasks: TaskDocument[] = await TaskModel.find();
+
+        return NextResponse.json({message: 'Success', tasks: allTasks})
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({message: 'Error fetching tasks'}, {status: 500})
+    }
+}
+
+export const dynamic = 'force-dynamic'
